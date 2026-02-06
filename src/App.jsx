b35327ee7b19743fe9379057141ab585e9d59a9e
Muhammadamin Zoirov <mzoirov4899@gmail.com>
2026-02-06 15:26:44 +0500

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css';
-import {createHashRouter, RouterProvider} from "react-router-dom";
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
 import RootPage from "./features/common/pages/RootPage/RootPage.jsx";
 import Dashboard from "./features/dashboard/pages/Dashboard/Dashboard.jsx";
 import News from "./features/news/pages/News/News.jsx";
@@ -14,9 +14,9 @@ import LivePage from "./features/live/page/LivePage/LivePage.jsx";
 import RatingPage from "./features/dashboard/pages/RatingPage/RatingPage.jsx";
 import GamePage from "./features/dashboard/pages/GamePage/GamePage.jsx";
 
-const router = createHashRouter([
+const router = createBrowserRouter([
     {
-        path: '/', element: <RootPage/>, children: [
+        path: '/UzChess_Frontend', element: <RootPage/>, children: [
             {index: true, element: <Dashboard/>},
             {path: '/rating', element: <RatingPage/>},
             {path: '/game', element: <GamePage/>},
