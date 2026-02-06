a1fe8f4d1aeec24843bcdd6ef7ea525bc3373d21
Muhammadamin Zoirov <mzoirov4899@gmail.com>
2026-02-06 15:18:24 +0500

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css';
-import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import {createHashRouter, RouterProvider} from "react-router-dom";
 import RootPage from "./features/common/pages/RootPage/RootPage.jsx";
 import Dashboard from "./features/dashboard/pages/Dashboard/Dashboard.jsx";
 import News from "./features/news/pages/News/News.jsx";
@@ -14,7 +14,7 @@ import LivePage from "./features/live/page/LivePage/LivePage.jsx";
 import RatingPage from "./features/dashboard/pages/RatingPage/RatingPage.jsx";
 import GamePage from "./features/dashboard/pages/GamePage/GamePage.jsx";
 
-const router = createBrowserRouter([
+const router = createHashRouter([
     {
         path: '/', element: <RootPage/>, children: [
             {index: true, element: <Dashboard/>},
